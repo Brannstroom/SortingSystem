95f7cb830c33c6eef0d785dd8bd92e664064fad6
Baktus79 <martinsen@hotmail.com>
2022-10-26 21:40:05 +0200

    Make hoppers filter on items pick up

--- src/main/java/me/brannstrom/sortingsystem/listeners/HopperListener.java
+++ src/main/java/me/brannstrom/sortingsystem/listeners/HopperListener.java
@@ -26,6 +26,7 @@ public class HopperListener implements Listener {
 	public void onInventoryMove(final InventoryMoveItemEvent event) {
 
 		final ItemStack item = event.getItem();
+		final Inventory destination = event.getDestination();
 
 		// Prevent the filter itself from being moved out of the hopper into another container, or from a container into a hopper.
 		if(event.getSource().getType() == InventoryType.HOPPER || event.getDestination().getType() == InventoryType.HOPPER) {
@@ -35,15 +36,79 @@ public class HopperListener implements Listener {
 			}
 		}
 
-		if(event.getSource().getType() == InventoryType.HOPPER) {
-			final Inventory destination = event.getDestination();
+		event.setCancelled(!shouldCollect(event.getSource().getType(), destination, item));
+	}
+
+	// Prevent any filter item from being put into the first slot of the hopper by click and place or shift-clicking
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent event){
+		if(event.getClickedInventory() != null) {
+			if(event.getView().getTopInventory().getType().equals(InventoryType.HOPPER)) {
+				if (event.getAction().equals(InventoryAction.MOVE_TO_OTHER_INVENTORY)) {
+					if(isFilter(event.getCurrentItem())) {
+						if(event.getView().getTopInventory().getContents()[0] == null) {
+							event.setCancelled(true);
+						}
+					}
+				}
+				else if (event.getAction().equals(InventoryAction.PLACE_ALL)) {
+					if (event.getSlot() == 0) {
+						if (isFilter(event.getCursor())) {
+							event.setCancelled(true);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Prevent any filter item from being dragged into the first slot of the hopper
+	@EventHandler
+	public void onInventoryDrag(InventoryDragEvent event) {
+		if(isFilter(event.getOldCursor())) {
+			if(event.getView().getTopInventory().getType() == InventoryType.HOPPER) {
+				if(event.getRawSlots().contains(0)) {
+					event.setCancelled(true);
+				}
+			}
+		}
+	}
+
+	// Prevent the hopper from picking up any filter item that is laying on top of it
+	@EventHandler
+	public void onInventoryPickupItem(final InventoryPickupItemEvent event) {
+		if(event.getInventory().getType() == InventoryType.HOPPER) {
+			if(isFilter(event.getItem().getItemStack())) {
+				event.setCancelled(true);
+			}
+
+			event.setCancelled(!shouldCollect(event.getInventory().getType(), event.getInventory(), event.getItem().getItemStack()));
+		}
+	}
+
+	// Check if the item is a filter item based on it's display name
+	private boolean isFilter(ItemStack item) {
+		if(item.hasItemMeta()) {
+			if(item.getItemMeta().hasDisplayName()) {
+				final String displayName = StringHandler.plainText(item.getItemMeta().displayName());
+				if(displayName.equalsIgnoreCase("filter") || displayName.equalsIgnoreCase("unfilter") || displayName.equalsIgnoreCase("filterexact")) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean shouldCollect(InventoryType sourcetype, Inventory dest, ItemStack item) {
+		// boolean to check if the items should be moved or not based on the filters in the destination inventory.
+		boolean allowThrough = true;
+
+		if(sourcetype == InventoryType.HOPPER) {
+
 
 			// Getting all the filters in the destination inventory. If there are none, we don't need to do anything.
-			final List<ItemStack> filters = getFilters(destination);
-			if(filters.size() == 0) return;
-
-			// boolean to check if the items should be moved or not based on the filters in the destination inventory.
-			boolean allowThrough = true;
+			final List<ItemStack> filters = getFilters(dest);
+			if(filters.size() == 0) return true;
 
 			// alreadyFiltered is used to check if the item has already been checked and should be allowed through, making sure the allowThrough boolean is not overwritten to false.
 			boolean alreadyFiltered = false;
@@ -89,66 +154,9 @@ public class HopperListener implements Listener {
 					}
 				}
 			}
-			event.setCancelled(!allowThrough);
 		}
-	}
 
-	// Prevent any filter item from being put into the first slot of the hopper by click and place or shift-clicking
-	@EventHandler
-	public void onInventoryClick(InventoryClickEvent event){
-		if(event.getClickedInventory() != null) {
-			if(event.getView().getTopInventory().getType().equals(InventoryType.HOPPER)) {
-				if (event.getAction().equals(InventoryAction.MOVE_TO_OTHER_INVENTORY)) {
-					if(isFilter(event.getCurrentItem())) {
-						if(event.getView().getTopInventory().getContents()[0] == null) {
-							event.setCancelled(true);
-						}
-					}
-				}
-				else if (event.getAction().equals(InventoryAction.PLACE_ALL)) {
-					if (event.getSlot() == 0) {
-						if (isFilter(event.getCursor())) {
-							event.setCancelled(true);
-						}
-					}
-				}
-			}
-		}
-	}
-
-	// Prevent any filter item from being dragged into the first slot of the hopper
-	@EventHandler
-	public void onInventoryDrag(InventoryDragEvent event) {
-		if(isFilter(event.getOldCursor())) {
-			if(event.getView().getTopInventory().getType() == InventoryType.HOPPER) {
-				if(event.getRawSlots().contains(0)) {
-					event.setCancelled(true);
-				}
-			}
-		}
-	}
-
-	// Prevent the hopper from picking up any filter item that is laying on top of it
-	@EventHandler
-	public void onInventoryPickupItem(final InventoryPickupItemEvent event) {
-		if(event.getInventory().getType() == InventoryType.HOPPER) {
-			if(isFilter(event.getItem().getItemStack())) {
-				event.setCancelled(true);
-			}
-		}
-	}
-
-	// Check if the item is a filter item based on it's display name
-	private boolean isFilter(ItemStack item) {
-		if(item.hasItemMeta()) {
-			if(item.getItemMeta().hasDisplayName()) {
-				final String displayName = StringHandler.plainText(item.getItemMeta().displayName());
-				if(displayName.equalsIgnoreCase("filter") || displayName.equalsIgnoreCase("unfilter") || displayName.equalsIgnoreCase("filterexact")) {
-					return true;
-				}
-			}
-		}
-		return false;
+		return allowThrough;
 	}
 
 	// Get all the filters in the inventory (hopper) and return them in a list
